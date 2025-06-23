b09a300957012bbae3dba918e647def422e0de20
ialexeyev <98263107+ialexeyev@users.noreply.github.com>
2025-06-23 06:25:25 +0000

    3. Web service set.

--- static/asset/main.js
+++ static/asset/main.js
@@ -10,14 +10,4 @@ function displayUserImg(img) {
       img.style.translate = "-3vw 3vw";
       img.style.scale = "5";
     }
-   
-    /*
-    img.style.width = "20vw";
-    img.style.height = "auto"; 
-    changedWidth = img.offsetWidth;
-    if (changedWidth == initialWidth) {
-      img.style.height = "60%";
-      img.style.width = "auto";
-    }
-    */
 }
